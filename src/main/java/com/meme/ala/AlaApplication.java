972377306e7788198e955d832f5d82505fc2fddd
Jung Jongmin <sdr2936@gmail.com>
2021-07-08 21:20:37 +0900

    Feat: 프로젝트 기본 세팅
    - gradle 의존성
    - 프로젝트 기본 틀

---
+++ src/main/java/com/meme/ala/AlaApplication.java
@@ -0,0 +1,13 @@
+package com.meme.ala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meme/ala/AlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meme.ala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
