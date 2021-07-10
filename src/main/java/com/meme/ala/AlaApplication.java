86cb52dfd0c684104d86d34971acd4620accefbd
Jung Jongmin <sdr2936@gmail.com>
2021-07-10 21:42:50 +0900

    Chore: CD/CI 세팅

--- src/main/java/com/meme/ala/AlaApplication.java
+++ src/main/java/com/meme/ala/AlaApplication.java
@@ -2,12 +2,21 @@ package com.meme.ala;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AlaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AlaApplication.class, args);
+    }
+
+    @GetMapping("/ping")
+    public ResponseEntity<?> ping(){
+        return ResponseEntity.ok("pong");
     }
 
 }
