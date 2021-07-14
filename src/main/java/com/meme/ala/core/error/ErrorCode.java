c37b381a19092a8ef05939e9ad148ba33408a96d
catnap421 <wlq7410@naver.com>
2021-07-14 22:07:07 +0900

    Chore: 쓰이지 않는 ErrorCode 삭제

--- src/main/java/com/meme/ala/core/error/ErrorCode.java
+++ src/main/java/com/meme/ala/core/error/ErrorCode.java
@@ -6,21 +6,15 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 public enum ErrorCode {
 
     // Common
-    INVALID_INPUT_VALUE(400, "C001", " Invalid Input Value"),
-    METHOD_NOT_ALLOWED(405, "C002", " Invalid Input Value"),
-    ENTITY_NOT_FOUND(400, "C003", " Entity Not Found"),
+    INVALID_INPUT_VALUE(400, "C001", "Invalid Input Value"),
+    METHOD_NOT_ALLOWED(405, "C002", "Method Not Allowed"),
+    ENTITY_NOT_FOUND(400, "C003", "Entity Not Found"),
     INTERNAL_SERVER_ERROR(500, "C004", "Server Error"),
     INVALID_TYPE_VALUE(400, "C005", " Invalid Type Value"),
     HANDLE_ACCESS_DENIED(403, "C006", "Access is Denied"),
 
-
     // Member
-    EMAIL_DUPLICATION(400, "M001", "Email is Duplication"),
-    LOGIN_INPUT_INVALID(400, "M002", "Login input is invalid"),
-
-    // Coupon
-    COUPON_ALREADY_USE(400, "CO001", "Coupon was already used"),
-    COUPON_EXPIRE(400, "CO002", "Coupon was already expired")
+    EMAIL_DUPLICATION(400, "M001", "Email is Duplication")
 
     ;
     private final String code;
