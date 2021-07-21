eaf99334dec021ee23b1087cf0b557912eafc84e
Jung Jongmin <sdr2936@gmail.com>
2021-07-21 11:55:47 +0900

    Test: MemberControllerTest 및 RestDocs 작성

--- src/test/java/com/meme/ala/member/controller/MemberControllerTest.java
+++ src/test/java/com/meme/ala/member/controller/MemberControllerTest.java
@@ -1,40 +1,52 @@
 package com.meme.ala.member.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.meme.ala.common.AbstractControllerTest;
 import com.meme.ala.core.auth.jwt.JwtProvider;
-import com.meme.ala.core.auth.oauth.OAuthProvider;
-import com.meme.ala.domain.member.model.entity.Member;
-import com.meme.ala.domain.member.service.MemberService;
-import com.meme.ala.member.entity.MemberCreator;
+import com.meme.ala.core.config.AlaWithAccount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.util.Map;
 
 import static com.meme.ala.core.config.ApiDocumentUtils.getDocumentRequest;
 import static com.meme.ala.core.config.ApiDocumentUtils.getDocumentResponse;
-import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class MemberControllerTest extends AbstractControllerTest {
-    @MockBean
+    @Autowired
     JwtProvider jwtProvider;
-
+    @MockBean
+    UserDetails userDetails;
+    @AlaWithAccount("test@gmail.com")
     @DisplayName("사용자 세팅 정보를 읽어오는 테스트")
     @Test
     public void 사용자_세팅_정보를_읽기_유닛테스트() throws Exception{
-        Member member= MemberCreator.testMember;
-        mockMvc.perform(get("/api/v1/member/me"));
+        mockMvc.perform(get("/api/v1/member/me"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.nickname").value("testNickname"))
+                .andDo(print())
+                .andDo(document("api/v1/member/me",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        responseFields(
+                                fieldWithPath("status").description("응답 상태"),
+                                fieldWithPath("message").description("설명"),
+                                fieldWithPath("data").description("사용자 세팅 정보"),
+                                fieldWithPath("data.email").description("사용자 이메일"),
+                                fieldWithPath("data.nickname").description("사용자 닉네임"),
+                                fieldWithPath("data.statusMessage").description("사용자 상태메시지"),
+                                fieldWithPath("data.imgUrl").description("사용자 이미지 사진 URL"),
+                                fieldWithPath("data.isOpen").description("사용자 공개 여부"),
+                                fieldWithPath("timestamp").description("타임스탬프")
+                        )
+                ));
     }
 }
 
