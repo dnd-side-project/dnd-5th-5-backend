3741357009478f9d18f41f757d047ff021d66daa
Jung Jongmin <sdr2936@gmail.com>
2021-07-26 12:56:17 +0900

    Fix: CORS Issue

---
+++ src/main/java/com/meme/ala/core/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.meme.ala.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+    }
+}
