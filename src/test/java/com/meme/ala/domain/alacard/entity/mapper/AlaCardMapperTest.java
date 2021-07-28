617ccac0f08be8fd6061fa641e740646dbe5e591
Jung Jongmin <sdr2936@gmail.com>
2021-07-29 02:59:18 +0900

    Feat: 사용자의 카드리스트로 글을 만들어서 제공
    - Test Codes
    - 사용자가 join을 할때 Aggregation을 초기화 해주도록 하였음(초기할당된 카드의 단어들과 카운트는 0)
    - 사용자의 카드리스트로 문장을 만드는 기능
    - 문장을 만들기 위해 각 중분류에서 가장 많은 선택을 받은 단어들을 고르는 기능
    - 가장 많이 선택을 받은 단어를 판별하기 위해 사용자로부터 Aggregation을 가져와 처리하는 기능

--- src/test/java/com/meme/ala/domain/alacard/entity/mapper/AlaCardMapperTest.java
+++ src/test/java/com/meme/ala/domain/alacard/entity/mapper/AlaCardMapperTest.java
@@ -2,43 +2,37 @@ package com.meme.ala.domain.alacard.entity.mapper;
 
 import com.meme.ala.common.DtoFactory;
 import com.meme.ala.common.EntityFactory;
-import com.meme.ala.domain.alacard.model.dto.request.AlaCardSaveDto;
-import com.meme.ala.domain.alacard.model.dto.response.SelectionWordDto;
+import com.meme.ala.domain.aggregation.model.entity.WordCount;
+import com.meme.ala.domain.alacard.model.dto.response.AlaCardDto;
 import com.meme.ala.domain.alacard.model.entity.AlaCard;
-import com.meme.ala.domain.alacard.model.entity.Word;
-import com.meme.ala.domain.alacard.model.mapper.AlaCardSaveMapper;
+import com.meme.ala.domain.alacard.model.mapper.AlaCardMapper;
+import com.meme.ala.domain.member.model.entity.cardSetting.AlaCardSetting;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class AlaCardMapperTest {
-
     @Autowired
-    private AlaCardSaveMapper alaCardSaveMapper;
+    private AlaCardMapper alaCardMapper;
 
-    private final AlaCardSaveDto dto = DtoFactory.alaCardSaveDto();
     private final AlaCard alaCard = EntityFactory.testAlaCard();
+    private final AlaCardSetting alaCardSetting = EntityFactory.testAlaCardSetting();
+    private final AlaCardDto alaCardDto = DtoFactory.testAlaCardDto();
+    private final List<WordCount> wordCountList = new LinkedList<WordCount>(Collections.singletonList(EntityFactory.testWordCount()));
 
     @Test
-    void DTO로부터_엔티티_업데이트_테스트(){
-        AlaCard mappedEntity = alaCardSaveMapper.toEntity(dto);
-        assertEquals(dto.getBigCategory(),mappedEntity.getBigCategory());
-        assertEquals(dto.getMiddleCategoryList().get(0).getMiddleCategoryName(),
-                mappedEntity.getMiddleCategoryList().get(0).getMiddleCategoryName());
-    }
-    @Test
-    void 엔티티로부터_SelectionWordDto리스트_변환_테스트(){
-        List<SelectionWordDto> dtoList = alaCardSaveMapper.alaCardToSelectionWordDtoList(alaCard);
-        assertEquals(dtoList.get(0).getWordName(),alaCard.getMiddleCategoryList().get(0).getWordList().get(0).getWordName());
-        assertEquals(dtoList.get(0).getBigCategory(),alaCard.getBigCategory());
-        assertEquals(dtoList.get(0).getHint(),alaCard.getMiddleCategoryList().get(0).getHint());
+    void 엔티티에서_DTO변환_테스트() {
+        AlaCardDto mappedDto = alaCardMapper.toDto(alaCard, alaCardSetting, "testSentence", wordCountList);
+        assertEquals(mappedDto.getSelectedWordList().get(0).getWordName(), alaCardDto.getSelectedWordList().get(0).getWordName());
+        assertEquals(mappedDto.getAlaCardSettingDto().getFont(), alaCardDto.getAlaCardSettingDto().getFont());
+        assertEquals(mappedDto.getAlaCardSettingDto().getFontColor(), alaCardDto.getAlaCardSettingDto().getFontColor());
+        assertEquals(mappedDto.getAlaCardSettingDto().getBackgroundColor(), alaCardDto.getAlaCardSettingDto().getBackgroundColor());
     }
 }
