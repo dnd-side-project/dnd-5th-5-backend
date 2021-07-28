5bd6a5ce315fc634fc2e64ce1ce8e579934643ec
Jung Jongmin <sdr2936@gmail.com>
2021-07-28 10:05:21 +0900

    Fix: Object ID 수정

--- src/main/java/com/meme/ala/domain/member/model/entity/cardSetting/AlaCardSetting.java
+++ src/main/java/com/meme/ala/domain/member/model/entity/cardSetting/AlaCardSetting.java
@@ -3,6 +3,8 @@ package com.meme.ala.domain.member.model.entity.cardSetting;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -11,6 +13,9 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 @Document("ALACARDSETTING")
 public class AlaCardSetting {
+    @Id
+    private ObjectId id;
+
     @DBRef
     private Background background;
 
