9a07defe5bbdfcd8aea880477279c67c82f56197
Jung Jongmin <sdr2936@gmail.com>
2021-08-04 18:31:14 +0900

    Fix: Timezone 수정

--- deploy.sh
+++ deploy.sh
@@ -32,4 +32,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
