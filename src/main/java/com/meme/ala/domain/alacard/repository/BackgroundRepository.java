16120dd666504158ddab2cec4c79b33e424a91cb
Jung Jongmin <sdr2936@gmail.com>
2021-08-05 11:02:22 +0900

    Feat: AlaCardSetting에 Background가 Ref가 아닌 Embedded로 들어가도록 변경

--- src/main/java/com/meme/ala/domain/alacard/repository/BackgroundRepository.java
+++ src/main/java/com/meme/ala/domain/alacard/repository/BackgroundRepository.java
@@ -4,5 +4,8 @@ import com.meme.ala.domain.alacard.model.entity.cardSetting.Background;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface BackgroundRepository extends MongoRepository<Background, ObjectId> {
+    List<Background> findAll();
 }
