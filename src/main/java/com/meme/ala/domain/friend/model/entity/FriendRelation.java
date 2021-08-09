ff8702838d6c96b261223833a71a10942b4e1498
catnap421 <wlq7410@naver.com>
2021-08-09 12:54:59 +0900

    Feat: FriendRelation의 한글 관계를 얻어올 수 있도록 함

--- src/main/java/com/meme/ala/domain/friend/model/entity/FriendRelation.java
+++ src/main/java/com/meme/ala/domain/friend/model/entity/FriendRelation.java
@@ -1,14 +1,17 @@
 package com.meme.ala.domain.friend.model.entity;
 
-import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 public enum FriendRelation {
     DEFAULT("일반"),
     FOLLOWING("팔로잉"),
     FOLLOWER("팔로워"),
     FRIEND("친구");
 
-    private FriendRelation(String relation){
-    }
+    private final String krRelation;
 
+    private FriendRelation(String krRelation){
+        this.krRelation = krRelation;
+    }
 }
