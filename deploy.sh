462edb6943f6c2cb07d48c3cbd3c52bff38a7dd2
Jung Jongmin <sdr2936@gmail.com>
2021-08-28 19:48:50 +0900

    Feat: 개발용 서버 배포

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,27 @@ if [ -z "$CURRENT_PID" ]; then
 else
     echo "> kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
+fi
+CURRENT_PID=$(pgrep -fl ala | awk '{print $1}')
+
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 10
+fi
+CURRENT_PID=$(pgrep -fl ala | awk '{print $1}')
+
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
     sleep 10
 fi
 
