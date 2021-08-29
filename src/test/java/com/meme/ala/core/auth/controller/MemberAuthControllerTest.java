54e5be20fd79e89b710f65c61474ce1a4e472e34
Jung Jongmin <sdr2936@gmail.com>
2021-08-29 23:57:55 +0900

    Feat: kakao user 로그인 기능 추가

--- src/test/java/com/meme/ala/core/auth/controller/MemberAuthControllerTest.java
+++ src/test/java/com/meme/ala/core/auth/controller/MemberAuthControllerTest.java
@@ -5,6 +5,7 @@ import com.meme.ala.common.AbstractControllerTest;
 import com.meme.ala.common.utils.NaverOauthUtil;
 import com.meme.ala.core.auth.jwt.JwtProvider;
 import com.meme.ala.core.auth.oauth.model.GoogleUser;
+import com.meme.ala.core.auth.oauth.model.KakaoUser;
 import com.meme.ala.core.auth.oauth.model.NaverUser;
 import com.meme.ala.core.auth.oauth.model.OAuthProvider;
 import com.meme.ala.core.auth.oauth.service.OAuthService;
@@ -90,6 +91,51 @@ public class MemberAuthControllerTest extends AbstractControllerTest {
                                 fieldWithPath("profileObj.name").description("사용자 이름"),
                                 fieldWithPath("profileObj.givenName").description("given name"),
                                 fieldWithPath("profileObj.familyName").description("family name")
+                        ),
+                        responseFields(
+                                fieldWithPath("status").description("응답 상태"),
+                                fieldWithPath("message").description("기존 사용자: LOGIN / 신규 사용자: JOIN"),
+                                fieldWithPath("data").description("JWT 값"),
+                                fieldWithPath("timestamp").description("타임스탬프")
+                        )
+                ));
+    }
+
+    @DisplayName("카카오 OAuth 로그인/가입 테스트")
+    @Test
+    public void 카카오_OAuth_로그인_유닛테스트() throws Exception {
+        String sampleRequestBody =
+                "{\n" +
+                        "  \"profileObj\": {\n" +
+                        "    \"kakaoId\": \"1155\",\n" +
+                        "    \"imageUrl\": \"https://user-images.githubusercontent.com/46064193/125324764-2bc8e200-e37b-11eb-8d07-9ac29d0d1b1a.png\",\n" +
+                        "    \"email\": \"test@gmail.com\",\n" +
+                        "    \"name\": \"Jongmin Jung\"\n" +
+                        "  }\n" +
+                        "}";
+
+        Map<String, Object> data = objectMapper.readValue(sampleRequestBody, Map.class);
+
+        when(oAuthService.getMemberByProvider(new ObjectMapper().readValue(sampleRequestBody, Map.class), OAuthProvider.KAKAO)).thenReturn(new KakaoUser(data));
+
+        when(memberService.existsProviderId(any())).thenReturn(Boolean.TRUE);
+
+        when(jwtProvider.createToken(any())).thenReturn("dummy token");
+
+        mockMvc.perform(post("/api/v1/oauth/jwt/kakao")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(sampleRequestBody))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data").value("dummy token"))
+                .andDo(print())
+                .andDo(document("api/v1/oauth/jwt/kakao",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestFields(
+                                fieldWithPath("profileObj.kakaoId").description("Identifier"),
+                                fieldWithPath("profileObj.imageUrl").description("사용자 프로필 링크"),
+                                fieldWithPath("profileObj.email").description("사용자 이메일"),
+                                fieldWithPath("profileObj.name").description("사용자 이름")
                         ),
                         responseFields(
                                 fieldWithPath("status").description("응답 상태"),
