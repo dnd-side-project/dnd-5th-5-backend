056f41b7aee8faab3887e7cf6e299e0056248d3c
Jung Jongmin <sdr2936@gmail.com>
2021-09-06 21:37:17 +0900

    Feat: 한글 분석 을/를 받침 유무 기능

--- src/main/java/com/meme/ala/domain/alacard/model/entity/MiddleCategory.java
+++ src/main/java/com/meme/ala/domain/alacard/model/entity/MiddleCategory.java
@@ -1,5 +1,6 @@
 package com.meme.ala.domain.alacard.model.entity;
 
+import com.meme.ala.common.utils.UnicodeUtil;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,4 +24,28 @@ public class MiddleCategory {
 
     @Singular("wordItem")
     private List<Word> wordList;
+
+    public String getParsedForm(String word) {
+        String delimeter = "/";
+        String[] formTokens = form.split(" ");
+        String baseToken = null;
+        String resultJosa;
+        if (!form.contains(delimeter))
+            return form.replace("???", word);
+        for (String token : formTokens) {
+            if (token.contains(delimeter) && token.contains("???")) {
+                baseToken = token;
+                break;
+            }
+        }
+        String josas = baseToken.split("\\?\\?\\?")[1];
+        String[] josaTokens = josas.split(delimeter);
+        if (UnicodeUtil.isWithBatchim(word))
+            resultJosa = josaTokens[0];
+        else
+            resultJosa = josaTokens[1];
+        String result = form.replace("???", word);
+        result = result.replace(josas, resultJosa);
+        return result;
+    }
 }
