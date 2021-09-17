33001b17f7022190be62844b4d16d84c589848ee
Jung Jongmin <sdr2936@gmail.com>
2021-09-17 23:59:10 +0900

    Fix: 테스트 및 type 수정

--- src/main/java/com/meme/ala/core/aop/FriendAspect.java
+++ src/main/java/com/meme/ala/core/aop/FriendAspect.java
@@ -2,7 +2,7 @@ package com.meme.ala.core.aop;
 
 import com.meme.ala.core.annotation.FlushFriendAlarm;
 import com.meme.ala.domain.alarm.service.AlarmService;
-import com.meme.ala.domain.member.model.entity.Member;
+import com.meme.ala.domain.friend.model.entity.FriendInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -24,10 +24,10 @@ public class FriendAspect {
 
     @AfterReturning(pointcut = "pointcut(flushFriendAlarm)", argNames = "joinPoint,flushFriendAlarm")
     public void afterReturning(JoinPoint joinPoint, FlushFriendAlarm flushFriendAlarm) {
-        Member member1 = (Member) joinPoint.getArgs()[0];
-        Member member2 = (Member) joinPoint.getArgs()[1];
-        alarmService.deleteAlarm(member1.getId(), member2.getId());
-        alarmService.deleteAlarm(member2.getId(), member1.getId());
+        FriendInfo friendInfo1 = (FriendInfo) joinPoint.getArgs()[0];
+        FriendInfo friendInfo2 = (FriendInfo) joinPoint.getArgs()[1];
+        alarmService.deleteAlarm(friendInfo1.getMemberId(), friendInfo2.getMemberId());
+        alarmService.deleteAlarm(friendInfo2.getMemberId(), friendInfo1.getMemberId());
     }
 }
 
